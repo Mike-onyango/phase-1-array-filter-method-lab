6cbed484f8a217d389887e7c503653717b28ccfe
mikey <michaelmykie71@gmail.com>
2024-04-01 23:52:37 +0300

    filtermethod

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, target) {
+    return drivers.filter(driver => driver.toLowerCase() === target.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  function matchName(drivers, target) {
+    return drivers.filter(driver => driver.name.toLowerCase() === target.toLowerCase());
+  }
